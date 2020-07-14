4af482dbd8c79ab9946125a070045b5f3e32f488
mrhb <mohammad_hajjar@yahoo.com>
2020-07-14 09:54:19 +0430

    javascript file decomposition

---
+++ OnlineMonitoringWeb/OnlineMonitoringWeb.Web/Scripts/adminlte/pages/Dashboard_Chart.js
@@ -0,0 +1,233 @@
+﻿
+
+/* *****************ChartJs هفتگی**********************/
+var timeFormat = 'MM/DD/YYYY HH:mm:ss';
+
+function newDate(days) {
+    return moment().add(days, 'd').toDate();
+}
+
+function newDateString(days) {
+    return moment().add(days, 'd').format(timeFormat);
+}
+var color = Chart.helpers.color;
+var dragOptions = {
+    animationDuration: 1000
+};
+var configb = {
+    type: 'line',
+    data: {
+        labels: [newDate(-1),
+        newDate()],
+        datasets: [{
+            label: 'Power',
+            backgroundColor: color(window.chartColors.green).alpha(0.5).rgbString(),
+            borderColor: window.chartColors.green,
+            fill: false,
+            data: [],
+        }]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Chart.js Time Scale'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    parser: timeFormat,
+                    // round: 'day'
+                    tooltipFormat: 'll HH:mm'
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: ''
+                },
+                ticks: {
+                    maxRotation: 0
+                }
+            }],
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: '(Power(Kw'
+                }
+            }]
+        },
+        plugins: {
+            zoom: {
+                zoom: {
+                    enabled: true,
+                    drag: false,
+                    mode: 'x',
+                    speed: 0.1
+                }
+            }
+        }
+    }
+};
+
+configb.data.datasets.forEach(function (dataset) {
+    dataset.borderColor = randomColor(0.4);
+    dataset.backgroundColor = randomColor(0.5);
+    dataset.pointBorderColor = randomColor(0);
+    dataset.pointBackgroundColor = randomColor(0.7);
+    dataset.pointBorderWidth = 0.1;
+});
+
+///////*************CHartJs هفتگی*****************
+
+
+
+/* *****************ChartJs روزانه**********************/
+function randomColorFactor() {
+    return Math.round(Math.random() * 255);
+}
+function randomColor(opacity) {
+    return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')';
+}
+var config = {
+    type: 'line',
+    data: {
+        labels: [newDate(-1),
+        newDate()],
+        datasets: [{
+            label: 'Power',
+            backgroundColor: color(window.chartColors.green).alpha(0.5).rgbString(),
+            borderColor: window.chartColors.green,
+            fill: true,
+            data: [],
+        }]
+    },
+    options: {
+        title: {
+            text: 'Chart.js Time Scale'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        hover: {
+            mode: 'index',
+            intersect: false
+        },
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    parser: timeFormat,
+                    // round: 'day'
+                    tooltipFormat: 'll HH:mm:ss'
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: ''
+                }
+            }],
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: '(Power(Kw'
+                }
+            }]
+        },
+        plugins: {
+            zoom: {
+                zoom: {
+                    enabled: true,
+                    drag: false,
+                    mode: 'x',
+                    speed: 0.05
+                }
+            }
+        }
+    }
+
+
+
+};
+
+config.data.datasets.forEach(function (dataset) {
+    dataset.borderColor = randomColor(0.4);
+    dataset.backgroundColor = randomColor(0.5);
+    dataset.pointBorderColor = randomColor(0.7);
+    dataset.pointBackgroundColor = randomColor(0.5);
+    dataset.pointBorderWidth = 1;
+});
+
+window.onload = function () {
+    var ctx = document.getElementById('chart1').getContext('2d');
+    var charta = new Chart(ctx, config);
+
+
+
+    Refreshq();
+    function Refreshq() {
+        $.ajax({
+            url: 'dbnames',
+            dataType: 'json',
+            success: function (dataFromUrl) {
+                var dataset = charta.config.data.datasets[0];
+                var i = 0;
+                dataset.data = [];
+                for (i = 0; i < dataFromUrl.length; i++) {
+                    var obj = {
+                        x: moment(dataFromUrl[i].y).format(timeFormat),
+                        y: dataFromUrl[i].item1
+                    };
+                    dataset.data.push(obj);
+                }
+
+                charta.update();
+
+                setTimeout(function () {
+                    Refreshq();
+                }, 20000);
+            }
+        });
+
+
+
+        var ctxb = document.getElementById('chart2').getContext('2d');
+        var chartb = new Chart(ctxb, configb);
+
+
+
+        Refreshb();
+        function Refreshb() {
+            $.ajax({
+                url: 'dbnames2',
+                dataType: 'json',
+                success: function (dataFromUrlb) {
+                    var datasetb = chartb.config.data.datasets[0];
+                    var i = 0;
+                    datasetb.data = [];
+                    for (i = 0; i < dataFromUrlb.length; i++) {
+                        var obj = {
+                            x: moment(dataFromUrlb[i].y).format(timeFormat),
+                            y: dataFromUrlb[i].item1
+                        };
+                        datasetb.data.push(obj);
+                    }
+
+                    chartb.update();
+
+                    setTimeout(function () {
+                        Refreshb();
+                    }, 20000);
+                }
+            });
+
+        }
+
+
+    }
+};
+
+///////*************CHartJs*****************
---
+++ OnlineMonitoringWeb/OnlineMonitoringWeb.Web/Scripts/adminlte/pages/Dashborad_Map.js
@@ -0,0 +1,97 @@
+﻿
+    //Iran MAp
+    $(function () {
+
+        var map,
+            markerIndex = 0,
+            markersCoords = {};
+        var plants = [
+            { name: 'VAK', coords: [36.30184530368433, 59.600830078125], status: 'closed', offsets: [0, 2] },
+            { name: 'MZFR', coords: [39.45, -110], status: 'activeUntil2018', offsets: [0, 2] },
+            { name: 'AVR', coords: [39.2, -107], status: 'activeUntil2022' }
+        ];
+
+
+        new jvm.MultiMap({
+            container: $('#world-map'),
+            maxLevel: 2,
+            subMapsOptions: {
+                backgroundColor: "transparent",
+            },
+            mapUrlByCode: function (code, multiMap) {
+                return '/Scripts/jvectormap/iran_regions_map.js';
+            },
+            main: {
+                map: 'GeoRefIran',
+                zoomMax: 12,
+                focusOn: {
+                    region: 'iran',
+                    animate: true
+                },
+                backgroundColor: "transparent",
+                regionStyle: {
+                    initial: {
+                        fill: '#e4e4e4',
+                        "fill-opacity": 1,
+                        stroke: 3,
+                        "stroke-width": 0,
+                        "stroke-opacity": 1
+                    }
+                },
+                onRegionTipShow: function (e, el, code) {
+                    el.html(el.html() + "<img src=\"Content/site/slides/slide1.jpg\"  height=\"42\" width=\"42\"><br>"
+                        + "salam");
+                },
+
+                markers: plants.map(function (h) { return { name: h.name, latLng: h.coords } }),
+                labels: {
+                    markers: {
+                        render: function (index) {
+                            return plants[index].name;
+                        },
+                        offsets: function (index) {
+                            var offset = plants[index]['offsets'] || [0, 0];
+
+                            return [offset[0] - 7, offset[1] + 3];
+                        }
+                    }
+                },
+                series: {
+                    markers: [{
+                        attribute: 'image',
+                        scale: {
+                            'closed': 'Content/map/icon-np-1.png',
+                            'activeUntil2018': 'Content/map/icon-np-2.png',
+                            'activeUntil2022': 'Content/map/icon-np-3.png'
+                        },
+                        values: plants.reduce(function (p, c, i) { p[i] = c.status; return p }, {}),
+                    }]
+                }
+            }
+        });
+    });
+    //World map by jvectormap
+    //$('#world-map').vectorMap({
+    //    map: 'iran_Region_mill_en',
+    //  backgroundColor: "transparent",
+    //  regionStyle: {
+    //    initial: {
+    //      fill: '#e4e4e4',
+    //      "fill-opacity": 1,
+    //      stroke: 'none',
+    //      "stroke-width": 0,
+    //      "stroke-opacity": 1
+    //    }
+    //  },
+    //  series: {
+    //    regions: [{
+    //      values: visitorsData,
+    //      scale: ["#92c1dc", "#ebf4f9"],
+    //      normalizeFunction: 'polynomial'
+    //    }]
+    //  },
+    //  onRegionLabelShow: function (e, el, code) {
+    //    if (typeof visitorsData[code] != "undefined")
+    //      el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
+    //  }
+    //});
