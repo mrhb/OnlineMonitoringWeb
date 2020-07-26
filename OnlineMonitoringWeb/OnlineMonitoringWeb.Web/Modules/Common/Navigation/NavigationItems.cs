69bf7ac27919f2f25ea700a0a53d37d6723f6f8d
mrhb <mohammad_hajjar@yahoo.com>
2020-07-26 10:03:29 +0430

    add UnitDashboard navigations

--- OnlineMonitoringWeb/OnlineMonitoringWeb.Web/Modules/Common/Navigation/NavigationItems.cs
+++ OnlineMonitoringWeb/OnlineMonitoringWeb.Web/Modules/Common/Navigation/NavigationItems.cs
@@ -2,3 +2,4 @@
 using Administration = OnlineMonitoringWeb.Administration.Pages;
 
 [assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "fa-tachometer")]
+[assembly: NavigationLink(900, "داشبورد موتور-ژنراتورها", url: "~/Dashboards/", permission: "", icon: "fa-tachometer")]
